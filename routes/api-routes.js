425f4521534f3e1f0bfb2143d4cf96a0f20be4c0
pebbbles <alondra@Kiaras-MBP.routerlogin.net>
2024-05-04 16:41:40 -0400

    fixed routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,30 +4,30 @@ const fs = require ("fs");
 
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
 router.get('/api/notes', async (req, res) => {
-  const dbJson = await JSON.parse(fs.readFileSync("db/db.json","utf8"));
+  const dbJson = await JSON.parse(fs.readFileSync("develop/db/db.json","utf8"));
   res.json(dbJson);
 });
 
 //
 router.post('/api/notes', (req, res) => {
-  const dbJson = JSON.parse(fs.readFileSync("db/db.json","utf8"));
+  const dbJson = JSON.parse(fs.readFileSync("develop/db/db.json","utf8"));
   const newFeedback = {
     title: req.body.title,
     text: req.body.text,
     id: uuidv4(),
   };
   dbJson.push(newFeedback);
-  fs.writeFileSync("db/db.json",JSON.stringify(dbJson));
+  fs.writeFileSync("develop/db/db.json",JSON.stringify(dbJson));
   res.json(dbJson);
 });
-
+// Delete request: Bonus
 router.delete('/api/notes/:id', (req, res) => {
-  let data = fs.readFileSync("db/db.json", "utf8");
+  let data = fs.readFileSync("develop/db/db.json", "utf8");
   const dataJSON =  JSON.parse(data);
   const newNotes = dataJSON.filter((note) => { 
     return note.id !== req.params.id;
   });
-  fs.writeFileSync("db/db.json",JSON.stringify(newNotes));
+  fs.writeFileSync("develop/db/db.json",JSON.stringify(newNotes));
   res.json("Note deleted.");
 });
 
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const api_routes = require('./routes/api-routes');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-
+const uuid = require('uuid');
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -17,5 +17,5 @@ app.use(api_routes);
 app.use(html_routes);
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}!`);
+  console.log(`Server running on http://localhost:${PORT}!`);
 });
