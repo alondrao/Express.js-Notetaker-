001e1fab3e2e5fc6f99e8ae94d58dbf80f8f9c1d
pebbbles <alondra@Kiaras-MBP.routerlogin.net>
2024-05-04 16:02:51 -0400

    fixed routes and server.js

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 const express = require('express');
-
+const html_routes = require('./routes/html-routes');
+const api_routes = require('./routes/api-routes');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+
 
 // Middleware
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Serve static files
 app.use(express.static('public'));
 
 // Use apiRoutes
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use(api_routes);
+app.use(html_routes);
 
 app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
+  console.log(`Server running on port ${PORT}!`);
 });
